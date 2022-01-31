dccc376d5f8aa0e97923ebc97354a871ec3a3f21
Nicholas Yang <nicholas@rome.tools>
2022-01-31 16:50:38 +0100

    Parse as module

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 use rome_formatter::{format as format_code, FormatOptions, IndentStyle};
-use rslint_parser::parse_script;
+use rslint_parser::{parse_module};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn format(code: &str) -> String {
-    let tree = parse_script(code, 0).syntax();
+    let tree = parse_module(code, 0).syntax();
 
     let options = FormatOptions {
         indent_style: IndentStyle::Tab,
@@ -14,7 +14,6 @@ pub fn format(code: &str) -> String {
     format_code(options, &tree)
         // TODO: impl Error for FormatError
         .unwrap()
-        .as_code()
-        .to_string()
+        .into_code()
 }
 
